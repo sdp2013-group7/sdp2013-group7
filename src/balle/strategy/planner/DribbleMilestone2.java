78d5208bfabb3ca6a50bc7cb06930421c6b431a7
Craig Thomson <s1034617@watten.inf.ed.ac.uk>
2013-02-10 18:55:34 +0000

    altered the code for dribblemilestone2 so that it tries to dribble towards the goal

--- src/balle/strategy/planner/DribbleMilestone2.java
+++ src/balle/strategy/planner/DribbleMilestone2.java
@@ -19,7 +19,8 @@ public class DribbleMilestone2 extends AbstractPlanner {
 
 	private static final Logger LOG = Logger.getLogger(DribbleMilestone2.class);
 	private static Coord startPosition;
-	private static final double distToTravel = 0.35;
+	private static final double distToTravel = 20; // usually 0.35(close to
+													// 30cm)
 	private boolean reachedBall = false;
 	private boolean done = false;
 
@@ -29,95 +30,125 @@ public class DribbleMilestone2 extends AbstractPlanner {
 	public DribbleMilestone2() {
 
 		// initialise strategies that will be used in milestone2
-		goToBallSafeStrategy = new GoToBallNoGoals(new GoToObjectPFN(0.15f));
+		goToBallSafeStrategy = new GoToBallSafeProportional();
 	}
 
 	// method to stop strategies that would not usually stop themselves
 	@Override
 	public void stop(Controller controller) {
-		
+
 		goToBallSafeStrategy.stop(controller);
 	}
 
 	@Override
-	public void onStep(Controller controller, Snapshot snapshot) throws ConfusedException {
+	public void onStep(Controller controller, Snapshot snapshot)
+			throws ConfusedException {
 
-		if (done)
+		if (done) {
+
 			return;
-		
+		}
+
 		// get position of our robot
 		Robot ourRobot = snapshot.getBalle();
-		
+
 		// check if robot is actually on the pitch
 		if (ourRobot.getPosition() == null) {
-			
-		LOG.info("where am i?!?!?!");
-		return;
+
+			LOG.info("where am i?!?!?!");
+			return;
 		}
-		
+
 		// get position of the ball
 		Ball ball = snapshot.getBall();
-		
 
 		// when we have possession
 		if (ourRobot.possessesBall(ball)) {
-			
+
 			// if we reach ball stop
 			if (!reachedBall) {
-				
+
 				goToBallSafeStrategy.stop(controller);
 				reachedBall = true;
 			}
-			
+
 			LOG.info("Posess ball");
-			
-			// if we don't have a previous start position set current to be start position
+
+			// if we don't have a previous start position set current to be
+			// start position
 			if (startPosition == null) {
-				
+
 				startPosition = ourRobot.getPosition();
 				LOG.info(startPosition);
 			}
-			
-			
+
+			// get positions
 			Coord ballPosition = snapshot.getBall().getPosition();
 			
+
 			// get angle from where we are facing to ball
-			double angle = ourRobot.getAngleToTurnToTarget(ballPosition);
-			
+			double angleFromBall = ourRobot.getAngleToTurnToTarget(ballPosition);
+
 			// convert to degrees
-			angle = Math.toDegrees(angle) / 2;
-						
+			angleFromBall = Math.toDegrees(angleFromBall) / 2;
+
 			// if angle large (above 10 off) then turn to ball
-			if (angle > 10 || angle < -10) {
-				
-				controller.rotate((int) angle, 50);
-				LOG.info("Angle: " + (int) angle);
+			if (angleFromBall > 10 || angleFromBall < -10) {
+
+				controller.rotate((int) angleFromBall, 50);
+				LOG.info("Angle: " + (int) angleFromBall);
+			}
+
+			Coord robotPosition = ourRobot.getPosition();
+
+			// get distance from start position (where we got possession) to
+			// where robot is now
+			double dist = startPosition.dist(robotPosition);
+
+			Coord ownGoal = snapshot.getOpponentsGoal().getPosition();
+			LOG.info("OwnGoalCoord:----- " + ownGoal);
+			
+			
+			// get angle off of goal
+			double angleFromGoal = Math.toDegrees(ourRobot.getAngleToTurnToTarget(ownGoal));
+
+			// if angle large (above 10 off) then turn to goal
+			boolean angleLarge = angleFromGoal > 10 || angleFromGoal < -10;
+			
+			if (angleLarge) {
+
+				controller.rotate((int) angleFromGoal, 50);
+				LOG.info("Anglefromgoal: " + (int) angleFromGoal);
+				LOG.info("Rotating");
 			}
 			
-			Coord robotPosition = ourRobot.getPosition();
-			
-			// get distance from start position (where we got possession) to where robot is now
-			double dist = startPosition.dist(robotPosition);
-			
-			// drive forward
-			controller.setWheelSpeeds(150, 150);
-			
-			LOG.info("Distance travelled " + dist);
-			
-			// when distance driven reaches distance to travel then stop
-			if (dist > distToTravel) {
+			if (!angleLarge) {
+
+				LOG.info("Anglefromgoal: " + (int) angleFromGoal);
+				LOG.info("Dribbling!");
 				
 				controller.stop();
-				done = true;
+				// when distance driven reaches distance to travel then stop
+				if (dist > distToTravel) {
+
+					controller.stop();
+					done = true;
+				}
+				
+				// drive forward
+				controller.setWheelSpeeds(150, 150);
+				LOG.info("Distance travelled " + dist);
+				
+				
 			}
-		
-		} 
-		
+
+		}
+
 		// if robot is not near ball
-		 else {
+		else {
 
 			LOG.info("going to ball");
-			
+
 			// execute strategy to go to the ball
 			goToBallSafeStrategy.step(controller, snapshot);
 			// add visuals to camera stream
@@ -125,10 +156,11 @@ public class DribbleMilestone2 extends AbstractPlanner {
 		}
 	}
 
-	// Factory method to make DribbleMilestone2 appear in the list of strats in the simulator
+	// Factory method to make DribbleMilestone2 appear in the list of strats in
+	// the simulator
 	@FactoryMethod(designator = "DribbleMilestone2", parameterNames = {})
 	public static DribbleMilestone2 factoryMethod() {
-		
+
 		return new DribbleMilestone2();
 	}
 }
