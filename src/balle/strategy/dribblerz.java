136dfc08e2f0e4cef4ce2f4a2c8ef36a6020ef1c
GeorgiTsatsev <s1045049@sms.ed.ac.uk>
2013-02-26 10:15:18 -0800

    A very simple strategy that turns the dribblers on and off if we are in possesion of the ball

---
+++ src/balle/strategy/dribblerz.java
@@ -0,0 +1,47 @@
+package balle.strategy;
+
+import org.apache.log4j.Logger;
+
+import balle.controller.Controller;
+import balle.strategy.planner.AbstractPlanner;
+import balle.strategy.planner.DribbleMilestone2;
+import balle.world.Snapshot;
+import balle.world.objects.Ball;
+import balle.world.objects.Robot;
+
+public class dribblerz extends AbstractPlanner {
+
+	private static final Logger LOG = Logger.getLogger(DribbleMilestone2.class);
+	
+    @Override
+    protected void onStep(Controller controller, Snapshot snapshot) throws ConfusedException {
+    	    	
+    	// Robot variable holding the position of the robot.
+    	Robot ourRobot = snapshot.getBalle();    	
+    	// Ball variable holding the position of the ball.
+    	Ball ball = snapshot.getBall();
+    	
+    	/* Check if we posses the ball or cant see it then turn the dribblers on
+    	
+    	*/
+    	if (ourRobot.possessesBall(ball) || ball.getPosition()==null) {
+    		
+    		controller.dribblersOn();
+    		LOG.info("The dribblers are on");
+    		
+    	} else {
+    		
+    		controller.dribblersOff();
+    		LOG.info("The dribblers are off");
+    		
+    	}
+    	
+    }
+
+
+    @FactoryMethod(designator = "Simple Dribblers", parameterNames = {})
+    public static final dribblerz factory() {
+        return new dribblerz();
+    }
+
+}
