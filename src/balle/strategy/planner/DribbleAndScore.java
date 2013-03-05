9513938c67a235f04c6aa8eee349842150b2b447
Marcus Sanatan <s1039549@carmen.inf.ed.ac.uk>
2013-03-05 13:16:11 +0000

    Updated with the hack that gets the ball and orientate towards the goal and shoot

--- src/balle/strategy/planner/DribbleAndScore.java
+++ src/balle/strategy/planner/DribbleAndScore.java
@@ -9,6 +9,7 @@ import balle.controller.Controller;
 import balle.strategy.ConfusedException;
 import balle.strategy.FactoryMethod;
 import balle.strategy.Strategy;
+import balle.world.Coord;
 import balle.world.Snapshot;
 import balle.world.objects.Ball;
 import balle.world.objects.Goal;
@@ -25,6 +26,8 @@ public class DribbleAndScore extends AbstractPlanner {
 	private Ball ball;
 	private Goal goal;
 	private Strategy goToBallSafeStrategy;
+	private boolean hasBall = false;
+	private boolean done = false;
 
 	public DribbleAndScore() {
 		goToBallSafeStrategy = new GoToBallSafeProportional();
@@ -33,6 +36,7 @@ public class DribbleAndScore extends AbstractPlanner {
 	// method to stop strategies that would not usually stop themselves
 	@Override
 	public void stop(Controller controller) {
+		controller.dribblersOff();
 		goToBallSafeStrategy.stop(controller);
 	}
 
@@ -44,6 +48,10 @@ public class DribbleAndScore extends AbstractPlanner {
 		ourRobot = snapshot.getBalle();
 		ball = snapshot.getBall();
 		goal = snapshot.getOpponentsGoal();
+		
+		if (done) {
+			return;
+		}
 
 		// Check if robot is actually on the pitch
 		if (ourRobot.getPosition() == null) {
@@ -51,36 +59,54 @@ public class DribbleAndScore extends AbstractPlanner {
 			return;
 		}
 
-		if (ourRobot.possessesBall(ball)
-				&& ourRobot.canScoreNoOpposition(ball, goal)) {
-			LOG.info("Let's score a goal!");
+		if (hasBall && ourRobot.canScoreNoOpposition(ball, goal)) {
+			
+			LOG.info("Let's score a goal! and dribblers off");
 			controller.dribblersOff();
 			controller.kick();
+			done = true;
 
-		} else if (ourRobot.possessesBall(ball)
-				&& !ourRobot.canScoreNoOpposition(ball, goal)) {
+		} else if (ourRobot.possessesBall(ball)&& !ourRobot.canScoreNoOpposition(ball, goal) && !hasBall) {
 			
 			LOG.info("Moving to a better scoring location :)");
 			
+			// get angle we need to turn to have ball in the centre of robot
+			double angle = ourRobot.getAngleToTurnToTarget(goal.getPosition());
+
+			// convert to degrees
+			angle = Math.toDegrees(angle) / 2;
+
+			// if angle is large enough (greater than 10) then turn to face ball
+			if (angle > 10 || angle < -10) {
+
+				controller.rotate((int) angle, 50);
+				LOG.info("Angle: " + (int) angle);
+			}
+			
 			// Keep dribblers on
 			controller.dribblersOn();
+			LOG.info("dribblers on");
+			hasBall = true;
 			
 			// Use the strategy to go towards the ball
 			goToBallSafeStrategy.step(controller, snapshot);
 			
 			// "Add visuals to camera stream" personally unsure what this does
 			addDrawables(goToBallSafeStrategy.getDrawables());
+		} else if (hasBall && !ourRobot.canScoreNoOpposition(ball, goal)) {
+			LOG.info("Move forward a bit more");
+			controller.setWheelSpeeds(200, 200);
+			
 		} else {
-			
+
 			LOG.info("Going to the ball");
-			
+
 			// Use the strategy to go towards the ball
 			goToBallSafeStrategy.step(controller, snapshot);
-			
+
 			// "Add visuals to camera stream" personally unsure what this does
 			addDrawables(goToBallSafeStrategy.getDrawables());
 		}
-
 	}
 
 	// To make it a usable stand-alone strategy
