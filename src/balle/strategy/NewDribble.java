c8891432e87baa537930b36bb5f5ad538c35515b
GeorgiTsatsev <s1045049@sms.ed.ac.uk>
2013-03-15 13:46:40 +0000

    Made a few changes to the possession distance.

--- src/balle/strategy/NewDribble.java
+++ src/balle/strategy/NewDribble.java
@@ -1,12 +1,19 @@
 package balle.strategy;
 
+import java.awt.Color;
+
 import org.apache.log4j.Logger;
 import balle.controller.Controller;
+
+import balle.main.drawable.DrawableRectangularObject;
+import balle.strategy.executor.movement.GoToObjectPFN;
 import balle.strategy.planner.AbstractPlanner;
-import balle.strategy.planner.GoToBallSafeProportional;
+import balle.strategy.planner.GoToBallNoGoals;
+import balle.world.Line;
 import balle.world.Snapshot;
 import balle.world.objects.Ball;
 import balle.world.objects.Goal;
+import balle.world.objects.RectangularObject;
 import balle.world.objects.Robot;
 
 /* A strategy to get the ball away from the walls using the dribblers.
@@ -22,24 +29,37 @@ public class NewDribble extends AbstractPlanner {
 	private Robot ourRobot;
 	private Ball ball;
 	private Goal goal;
-	private Strategy goToBallSafeStrategy;
+	private Strategy goToBallSimple;
 	private boolean done = false;
     
-
+    public boolean hasBall(Ball ball) {
+        
+    	if ((ball.getPosition() == null) || (ourRobot.getPosition() == null))
+            return false;
+        
+        Line frontLine = ourRobot.getFrontSide().extendBothDirections(-0.02);
+		double distance = (frontLine.dist(ball.getPosition()));
+        
+		// Draw possession area
+        RectangularObject possessionRect = frontLine.widen(0.01);
+		addDrawable(new DrawableRectangularObject (possessionRect, Color.BLUE));
+		
+        return distance <= 0.01;
+    }
 	public NewDribble() {
-		goToBallSafeStrategy = new GoToBallSafeProportional();
+		goToBallSimple = new  GoToBallNoGoals(new GoToObjectPFN(0,true), false);
+		
 	}
 
 	// method to stop strategies that would not usually stop themselves
 	@Override
 	public void stop(Controller controller) {
 		controller.dribblersOff();
-		goToBallSafeStrategy.stop(controller);
+		goToBallSimple.stop(controller);
 	}
 	
 	@Override
 	protected void onStep(Controller controller, Snapshot snapshot) throws ConfusedException {
-		
 		
 		ourRobot = snapshot.getBalle();
 		ball = snapshot.getBall();
@@ -60,12 +80,12 @@ public class NewDribble extends AbstractPlanner {
 		}
 		
 		// If we dont have possession  of the ball go for it.
-		if (!ourRobot.possessesBall(ball)){
+		if (!hasBall(ball)){
 			LOG.info("We has no ball. I shall get it.");
-			goToBallSafeStrategy.step(controller, snapshot);
+			goToBallSimple.step(controller, snapshot);
 			// Adds the point of positioning of our robot while 
-			//executing the goToBallSafeStrategy.
-			addDrawables(goToBallSafeStrategy.getDrawables());
+			//executing the goToBallSimple.
+			addDrawables(goToBallSimple.getDrawables());
 		}
 		// Else turn the "dribblers" on and go backwards and we are done.
 		else {
@@ -109,4 +129,5 @@ public class NewDribble extends AbstractPlanner {
 	{
 		return new NewDribble();
 	} 
+	
 }
